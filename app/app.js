aea3ad69fed47782f5619adc7387eeadfdd1c3a9
Pedro Cunha <pedro.2112@hotmail.com.br>
2022-10-19 19:34:06 -0300

    Revert "Cli sequelize init"
    This reverts commit c3c4fa11546bfa03dbe28aadc85df743a726fac5.

--- app/app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const conectBd = require("./database/conectionBd");
-
-const app= express();
-
-app.use(express.json());
-
-module.exports=app;
--- app/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-let port= process.env.PORT || 3000;
-
-
-app.listen(port,()=>{
-    console.log(`Server rodando na porta ${port}`);
-})
